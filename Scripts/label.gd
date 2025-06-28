b69315878eed9777607f080e3353221e9de097e8
codefishyu <2811867887@qq.com>
2025-06-28 23:08:26 +0200

    添加屏幕缩放以及拖动，优化图层，添加元素费用以及暂停UI

--- Scripts/label.gd
+++ Scripts/label.gd
@@ -1,5 +1,7 @@
 extends Label
-@onready var scene: Node2D = $".."
+
+@onready var scene_node = get_tree().get_root().get_node("Main/Scene")
 
 func _process(delta: float) -> void:
-	self.text = str(scene.current_cost) + "c"
+	self.text = str(scene_node.current_cost) + "c"
+	self.modulate = Color.BLACK
