2268faf5a2e61625825a1d5d83a05b40c2b675d6
IngresW <149072133+IngresW@users.noreply.github.com>
2025-06-29 14:27:48 +0800

    Revert "Merge commit 'd56c6f0f04968f9ffeebcce3b949b492a2529977' into reverse"
    This reverts commit 4185eeb1c75906c6b6a007824d7cc8369cac8e9c, reversing
    changes made to 60de13b46c02f965eb6dbf7f5b928474787daed0.

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -9,11 +9,10 @@ func _ready():
 	#game_scene.visible = false
 	#tutorial.visible = false
 	#start_menu.visible = true
-	start_menu.visible = false
 	game_scene.visible = true
 	
 	#tutorial.set_process(false)
-	game_scene.set_process(false)
+	#game_scene.set_process(false)
 	return#等会再说
 
 func manage_scene():
@@ -23,10 +22,3 @@ func go_to_tutorial():
 	tutorial.visible = true
 	start_menu.visible = false
 	tutorial.set_process(true)
-	return
-	
-func go_to_main_game():
-	game_scene.visible = true
-	tutorial.visible = false
-	start_menu.visible = false
-	return
--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -1,5 +1,4 @@
 extends CanvasLayer
 
 func _on_start_button_pressed() -> void:
-	#get_parent().go_to_tutorial()
-	get_parent().go_to_main_game()
+	get_parent().go_to_tutorial()
