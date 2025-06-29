15fb32deaa533c2923bd7dd550dd4b8ce0faf6a1
ZhaoAn <zhaoan@ZhaoAndeMacBook-Air.local>
2025-06-29 13:35:05 +0800

    add score

--- Scripts/label.gd
+++ Scripts/label.gd
@@ -3,5 +3,6 @@ extends Label
 @onready var scene_node = get_tree().get_root().get_node("Main/Scene")
 
 func _process(delta: float) -> void:
-	self.text = str(scene_node.current_cost) + "c"
+	self.text = "Cost: " + str(scene_node.current_cost)
+	self.text += "\nScore: " + str(scene_node.score)
 	self.modulate = Color.BLACK
